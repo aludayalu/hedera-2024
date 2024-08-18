eb3625947125865adbd78a22edfd207f24c9e78c
aludayalu <pl46u3.ducklinz@gmail.com>
2024-08-18 23:04:06 +0530

    tailwind css made globally available in monster

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import sys, json
 
 app = Flask(__name__)
 
+tailwindcss="<script>"+open("public/tailwind.js").read()+"</script>"
+
 @app.get("/")
 def home():
     signals=open("public/signals.js").read()
     print(tokeniser(open("components/index.html").read()))
     print(parser(tokeniser(open("components/index.html").read())))
-    return render("index", locals())
+    return render("index", locals()|globals())
 
 app.run(host="0.0.0.0", port=int(sys.argv[1]))
